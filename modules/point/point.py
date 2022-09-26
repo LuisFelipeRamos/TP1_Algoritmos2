99477958f35f7ab5c584b8257b70a693c53f5ffd
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-09-26 19:52:29 -0300

    implementação do produto vetorial e reestruturação dos diretorios

--- modules/point/point.py
+++ modules/point/point.py
@@ -1,8 +1,14 @@
-
 class Point():
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    
+    def __lt__(self, other):
+        return self.y < other.y if self.y != other.y else self.x < other.x
+    
 
     
