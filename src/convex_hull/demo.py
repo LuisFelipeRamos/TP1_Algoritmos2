b435d46f552529aef10624f6e863927015528321
Igor <igorlfs@ufmg.br>
2022-09-28 20:22:08 -0300

    ref: estaticamente tipando tudo

--- src/convex_hull/demo.py
+++ src/convex_hull/demo.py
@@ -1,19 +1,27 @@
 from src.point import Point
+from src.segment import Segment
 from src.convex_hull import gift_wrapping_convex_hull
 import random
 import matplotlib.pyplot as plt
 
-set_of_points = []
+set_of_points: list[Point] = []
 for _ in range(100):
+    x: int
+    y: int
     x, y = random.randint(1, 100), random.randint(1, 100)
     set_of_points.append(Point(x, y))
 
-hull = gift_wrapping_convex_hull(set_of_points)
+hull: list[Segment] = gift_wrapping_convex_hull(set_of_points)
 
 
-xmin, xmax, ymin, ymax = -100, 100, -100, 100
-ticks_frequency = 1
+xmin: int = -100
+ymin: int = -100
+xmax: int = 100
+ymax: int = 100
+ticks_frequency: int = 1
+
 fig, ax = plt.subplots(figsize=(100, 100))
+
 ax.scatter(
     [point.x for point in set_of_points], [point.y for point in set_of_points], c=["b"]
 )
--- src/test/test_segment.py
+++ src/test/test_segment.py
@@ -3,7 +3,7 @@ from src.segment import Segment
 
 
 def test_is_counter_clockwise():
-    data = [
+    data: list[list[int]] = [
         [1, 1, 2, 2, 3, 4],
         [7, 0, 4, 2, 0, 3],
         [2, -4, 6, -4, 7, 1],
@@ -11,12 +11,15 @@ def test_is_counter_clockwise():
         [2, 1, 4, 4, 2, 6],
     ]
     for case in data:
-        points = [
+        points: list[Point] = [
             Point(case[0], case[1]),
             Point(case[2], case[3]),
             Point(case[4], case[5]),
         ]
-        segments = [Segment(points[0], points[1]), Segment(points[1], points[2])]
+        segments: list[Segment] = [
+            Segment(points[0], points[1]),
+            Segment(points[1], points[2]),
+        ]
 
         assert segments[0].is_counter_clockwise(segments[1]) == False
         assert segments[1].is_counter_clockwise(segments[0]) == True
