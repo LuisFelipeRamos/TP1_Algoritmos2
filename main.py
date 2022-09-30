27df134d15326a91e6e4458e509a1963c1b5f38b
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-09-30 19:28:51 -0300

    variando a aleatoriedade

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def generate_random_set_of_points(number_of_points, min_x, max_x, min_y, max_y):
     for _ in range(number_of_points):
         x: int
         y: int
-        x, y = random.randint(min_x, max_x), random.randint(min_y, max_y)
-        set_of_points.append(Point(x, y))
+        x, y = random.randint(min_x, max_x)*random.randint(min_x, max_x), random.randint(min_y, max_y)*random.randint(min_x, max_x)
+        set_of_points.append(Point(x/max_x, y/max_y))
 
     return set_of_points
 
@@ -24,11 +24,11 @@ def main():
     WIDTH: int = 600
     HEIGHT: int = 600
     set_of_points = generate_random_set_of_points(
-        50, SCREEN_MARGIN, WIDTH - SCREEN_MARGIN, SCREEN_MARGIN, HEIGHT - SCREEN_MARGIN
+        25, SCREEN_MARGIN, WIDTH - SCREEN_MARGIN, SCREEN_MARGIN, HEIGHT - SCREEN_MARGIN
     )
 
     alg_visualization = AlgorithmVisualization(WIDTH, HEIGHT, SCREEN_MARGIN, FPS)
-    alg_visualization.animate_convex_hull("graham_scan", set_of_points)
+    alg_visualization.animate_convex_hull("gift_wrapping", set_of_points)
 
 
 if __name__ == "__main__":
---
+++ src/convex_hull/analysis.py
@@ -0,0 +1,8 @@
+from .convex_hull import convex_hull
+from src.point import Point
+from src.segment import Segment
+
+# libs
+import matplotlib.pyplot as plt
+import random
+import math
