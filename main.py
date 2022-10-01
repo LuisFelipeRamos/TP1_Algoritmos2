69177a22e2c121bd243e533d30c0b448c90227ed
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-10-01 15:12:38 -0300

    incremental alg feito, salvo alguns problemas com pontos colieanres. Usando pontos aleatorios float diminui a chance de pontos colineares. Em breve temrino a animação

--- main.py
+++ main.py
@@ -5,31 +5,3 @@ import random
 
 from src.animation import AlgorithmVisualization
 from src.point.point import Point
-
-
-def generate_random_set_of_points(number_of_points, min_x, max_x, min_y, max_y):
-    set_of_points: list[Point] = []
-    for _ in range(number_of_points):
-        x: int
-        y: int
-        x, y = random.randint(min_x, max_x)*random.randint(min_x, max_x), random.randint(min_y, max_y)*random.randint(min_x, max_x)
-        set_of_points.append(Point(x/max_x, y/max_y))
-
-    return set_of_points
-
-
-def main():
-    FPS: int = 60
-    SCREEN_MARGIN: int = 100
-    WIDTH: int = 600
-    HEIGHT: int = 600
-    set_of_points = generate_random_set_of_points(
-        25, SCREEN_MARGIN, WIDTH - SCREEN_MARGIN, SCREEN_MARGIN, HEIGHT - SCREEN_MARGIN
-    )
-
-    alg_visualization = AlgorithmVisualization(WIDTH, HEIGHT, SCREEN_MARGIN, FPS)
-    alg_visualization.animate_convex_hull("gift_wrapping", set_of_points)
-
-
-if __name__ == "__main__":
-    main()
--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
+from src.convex_hull.convex_hull import ConvexHull
 from src.point.point import Point
 import random
+import math
 
 from src.animation import AlgorithmVisualization
 
@@ -9,24 +11,26 @@ def generate_random_set_of_points(number_of_points: int, min_x: int, max_x: int,
     for _ in range(number_of_points):
         x: int
         y: int
-        x, y = random.randint(min_x, max_x)*random.randint(min_x, max_x), random.randint(min_y, max_y)*random.randint(min_x, max_x)
-        set_of_points.append(Point(x/max_x, y/max_y))
+        x, y = random.uniform(min_x, max_x)*random.uniform(min_x, max_x), random.uniform(min_y, max_y)*random.uniform(min_x, max_x)
+        set_of_points.append(Point(int(math.sqrt(x)), int(math.sqrt(y))))
 
     return set_of_points
 
 
 def run():
     FPS: int = 60
-    SCREEN_MARGIN: int = 100
-    WIDTH: int = 600
-    HEIGHT: int = 600
+    SCREEN_MARGIN: int = 0
+    WIDTH: int = 100
+    HEIGHT: int = 100
     set_of_points = generate_random_set_of_points(
-        25, SCREEN_MARGIN, WIDTH - SCREEN_MARGIN, SCREEN_MARGIN, HEIGHT - SCREEN_MARGIN
+        146, SCREEN_MARGIN, WIDTH - SCREEN_MARGIN, SCREEN_MARGIN, HEIGHT - SCREEN_MARGIN
     )
 
-    alg_visualization = AlgorithmVisualization(WIDTH, HEIGHT, SCREEN_MARGIN, FPS)
-    alg_visualization.animate_convex_hull("gift_wrapping", set_of_points)
+    """ alg_visualization = AlgorithmVisualization(WIDTH, HEIGHT, SCREEN_MARGIN, FPS)
+    alg_visualization.animate_convex_hull("incremental_alg", set_of_points) """
 
+    convex_hull = ConvexHull(set_of_points, alg='incremental_alg')
+    convex_hull.plot(100)
 
 if __name__ == "__main__":
     run()
