080c0239bee0b1bfe30ed1db1273cd1a679b94aa
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2022-10-19 23:37:53 -0300

    iris petal separavel

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import random
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
 from typing import cast
 
@@ -97,8 +98,55 @@ def run():
     parser.add_argument("--file", dest="file", required=False, type=str, help="arquivo .dat que contêm dados a serem treinados")
     args = parser.parse_args()
 
-    sop1 = generate_random_set_of_points(20, 5, 10, 5, 10)
-    sop2 = generate_random_set_of_points(20, 15, 20, 15, 20)
+    col_names_iris = ["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Class"]
+    iris = pd.read_csv(args.file, names=col_names_iris)
+
+    irisclass1 = iris[iris["Class"] != " Iris-setosa"]
+    irisclass2 = iris[iris["Class"] == " Iris-setosa"]
+    irisclass1 = irisclass1[["PetalLength", "PetalWidth"]]
+    irisclass2 = irisclass2[["PetalLength", "PetalWidth"]]
+
+    irisclass1_train = irisclass1.sample(frac=0.7)
+    irisclass1_test = irisclass1.drop(irisclass1_train.index)
+    irisclass1_train.reset_index(drop=True, inplace=True)
+    irisclass1_test.reset_index(drop=True, inplace=True)
+
+    irisclass2_train = irisclass2.sample(frac=0.7)
+    irisclass2_test = irisclass2.drop(irisclass2_train.index)
+    irisclass2_train.reset_index(drop=True, inplace=True)
+    irisclass2_test.reset_index(drop=True, inplace=True)
+
+    list_irisclass1_train = []
+    for x in range(irisclass1_train["PetalLength"].size):
+        temp_point = Point(
+            irisclass1_train["PetalLength"][x], irisclass1_train["PetalWidth"][x]
+        )
+        list_irisclass1_train.insert(1, temp_point)
+
+    list_irisclass1_test = []
+    for x in range(irisclass1_test["PetalLength"].size):
+        temp_point = Point(
+            irisclass1_test["PetalLength"][x], irisclass1_test["PetalWidth"][x]
+        )
+        list_irisclass1_test.insert(1, temp_point)
+    
+    list_irisclass2_train = []
+    for x in range(irisclass2_train["PetalLength"].size):
+        temp_point = Point(
+            irisclass2_train["PetalLength"][x], irisclass2_train["PetalWidth"][x]
+        )
+        list_irisclass2_train.insert(1, temp_point)
+
+    list_irisclass2_test = []
+    for x in range(irisclass2_test["PetalLength"].size):
+        temp_point = Point(
+            irisclass2_test["PetalLength"][x], irisclass2_test["PetalWidth"][x]
+        )
+        list_irisclass2_test.insert(1, temp_point)
+
+
+    sop1 = list_irisclass1_train
+    sop2 = list_irisclass2_train
     ch1 = ConvexHull(sop1, alg="graham_scan")
     ch2 = ConvexHull(sop2, alg="graham_scan")
 
@@ -130,7 +178,7 @@ def run():
     plt.plot([min_dist_segment.p0.x, min_dist_segment.p1.x], [min_dist_segment.p0.y, min_dist_segment.p1.y], "yellow", linewidth=.8)
 
     slope, b = get_perpendicular_segment(min_dist_segment)
-    x = np.linspace(5, 20, 100)
+    x = np.linspace(3, 5, 100)
     y = slope*x + b
     plt.plot(x, y, "-r")
 
