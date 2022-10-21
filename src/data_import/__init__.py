1e2c44d0b5319b982b9e5a918baa68e9a094b0f9
Igor <igorlfs@ufmg.br>
2022-10-21 16:39:51 -0300

    chore: ajusta módulos

--- src/data_import/__init__.py
+++ src/data_import/__init__.py
@@ -1,1 +1,3 @@
 from .iris import check_iris
+
+__all__ = ["check_iris"]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,13 @@
-from .min_dist_between_conves_hulls_segment import min_dist_between_convex_hulls_segment
+from .calculate_times_and_plot import (
+    calculate_times_and_plot,
+    generate_random_set_of_points,
+)
 from .get_perpendicular_segment import get_perpendicular_segment
-from .calculate_times_and_plot import generate_random_set_of_points, calculate_times_and_plot
+from .min_dist_between_conves_hulls_segment import min_dist_between_convex_hulls_segment
+
+__all__ = [
+    "calculate_times_and_plot",
+    "generate_random_set_of_points",
+    "get_perpendicular_segment",
+    "min_dist_between_convex_hulls_segment",
+]
