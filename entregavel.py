25677cfd3a544db2979ca6ea9e58054793a5e718
Igor <igorlfs@ufmg.br>
2022-10-23 19:14:27 -0300

    feat: programa mínimo para testar etapas inicias

--- entregavel.py
+++ entregavel.py
@@ -1,6 +1,15 @@
+# pylint: skip-file
+# flake8: noqa
 from __future__ import annotations
 
 import math
+import random
+from typing import cast
+
+import matplotlib.pyplot as plt
+
+X = -math.inf
+
 
 class Point:
     def __init__(self, x: float, y: float) -> None:
@@ -36,8 +45,6 @@ class Point:
                 crossings += 1
         return (crossings % 2) != 0
 
-from math import inf
-X = -inf
 
 class Segment:
     def __init__(self, p0: Point, p1: Point) -> None:
@@ -156,10 +163,6 @@ class Segment:
         linear: float = midpoint.y - negative_inverse_slope * midpoint.x
         return negative_inverse_slope, linear, midpoint
 
-
-from typing import cast
-
-import matplotlib.pyplot as plt
 
 class ConvexHull:
     def __init__(self, set_of_points: list[Point], alg: str) -> None:
@@ -301,7 +304,7 @@ class ConvexHull:
         return convex_hull
 
     def plot(self) -> None:
-        _, ax = plt.subplots(figsize=(100, 100))
+        _, ax = plt.subplots()
         ax = cast(plt.Axes, ax)
         ax.scatter(
             [point.x for point in self.set_of_points],
@@ -312,7 +315,7 @@ class ConvexHull:
         ax.grid(which="both", color="grey", linewidth=0.5, linestyle="-", alpha=0.2)
         for edge in self.convex_hull:
             plt.plot([edge.p0.x, edge.p1.x], [edge.p0.y, edge.p1.y], "k", linewidth=0.5)
-        plt.show()
+        plt.savefig("envoltória.png")
 
     def is_inside(self, other: ConvexHull) -> bool:
         """
@@ -338,6 +341,7 @@ class ConvexHull:
                     min_dist = curr_dist
                     min_dist_segment = Segment(p, q)
         return min_dist_segment
+
 
 class Event:
     """
@@ -376,6 +380,7 @@ class Event:
 
     def __lt__(self, other: Event) -> bool:
         return self.compare(other) < 0
+
 
 class LineSweep:
     """
@@ -418,7 +423,7 @@ class LineSweep:
             segment_id: tuple[Segment, int] = (event.segment, event.identifier)
 
             if event.is_left:
-                g.X = segment_id[0].p0.x + eps
+                X = segment_id[0].p0.x + eps
                 tree_segments.insert(segment_id)
                 node = tree_segments.search(segment_id)
                 if node is not None:
@@ -494,3 +499,415 @@ class LineSweep:
                         break
                     node_iterator = node_iterator.parent
         return above, below
+
+
+class AVLNode:
+    def __init__(self, val):
+        self.val = val
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.height = 0
+
+    def isLeaf(self):
+        return self.height == 0
+
+    def maxChildrenHeight(self):
+        if self.left and self.right:
+            return max(self.left.height, self.right.height)
+        elif self.left and not self.right:
+            return self.left.height
+        elif not self.left and self.right:
+            return self.right.height
+        else:
+            return -1
+
+    def balanceFactor(self):
+        return (self.left.height if self.left else -1) - (
+            self.right.height if self.right else -1
+        )
+
+    def __str__(self):
+        return "AVLNode(" + str(self.val) + ", Height: %d )" % self.height
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+        self.rebalance_count = 0
+        self.nodes_count = 0
+
+    def setRoot(self, val):
+        """
+        Set the root value
+        """
+        self.root = AVLNode(val)
+
+    def countNodes(self):
+        return self.nodes_count
+
+    def getDepth(self):
+        """
+        Get the max depth of the BST
+        """
+        if self.root:
+            return self.root.height
+        else:
+            return -1
+
+    def findSmallest(self, start_node):
+        assert not start_node is None
+        node = start_node
+        while node.left:
+            node = node.left
+        return node
+
+    def findBiggest(self, start_node):
+        assert not start_node is None
+        node = start_node
+        while node.right:
+            node = node.right
+        return node
+
+    def insert(self, val):
+        """
+        insert a val into AVLTree
+        """
+        if self.root is None:
+            self.setRoot(val)
+        else:
+            self._insertNode(self.root, val)
+        self.nodes_count += 1
+
+    def _insertNode(self, currentNode, val):
+        """
+        Helper function to insert a value into AVLTree.
+        """
+        node_to_rebalance = None
+        if currentNode.val > val:
+            if currentNode.left:
+                self._insertNode(currentNode.left, val)
+            else:
+                child_node = AVLNode(val)
+                currentNode.left = child_node
+                child_node.parent = currentNode
+                if currentNode.height == 0:
+                    self._recomputeHeights(currentNode)
+                    node = currentNode
+                    while node:
+                        if node.balanceFactor() in [-2, 2]:
+                            node_to_rebalance = node
+                            break  # we need the one that is furthest from the root
+                        node = node.parent
+        else:
+            if currentNode.right:
+                self._insertNode(currentNode.right, val)
+            else:
+                child_node = AVLNode(val)
+                currentNode.right = child_node
+                child_node.parent = currentNode
+                if currentNode.height == 0:
+                    self._recomputeHeights(currentNode)
+                    node = currentNode
+                    while node:
+                        if node.balanceFactor() in [-2, 2]:
+                            node_to_rebalance = node
+                            break  # we need the one that is furthest from the root
+                        node = node.parent
+        if node_to_rebalance:
+            self._rebalance(node_to_rebalance)
+
+    def _rebalance(self, node_to_rebalance):
+        A = node_to_rebalance
+        F = A.parent  # allowed to be NULL
+        if A.balanceFactor() == -2:
+            if A.right.balanceFactor() <= 0:
+                B = A.right
+                C = B.right
+                assert not A is None and not B is None and not C is None
+                A.right = B.left
+                if A.right:
+                    A.right.parent = A
+                B.left = A
+                A.parent = B
+                if F is None:
+                    self.root = B
+                    self.root.parent = None
+                else:
+                    if F.right == A:
+                        F.right = B
+                    else:
+                        F.left = B
+                    B.parent = F
+                self._recomputeHeights(A)
+                self._recomputeHeights(B.parent)
+            else:
+                B = A.right
+                C = B.left
+                assert not A is None and not B is None and not C is None
+                B.left = C.right
+                if B.left:
+                    B.left.parent = B
+                A.right = C.left
+                if A.right:
+                    A.right.parent = A
+                C.right = B
+                B.parent = C
+                C.left = A
+                A.parent = C
+                if F is None:
+                    self.root = C
+                    self.root.parent = None
+                else:
+                    if F.right == A:
+                        F.right = C
+                    else:
+                        F.left = C
+                    C.parent = F
+                self._recomputeHeights(A)
+                self._recomputeHeights(B)
+        else:
+            assert node_to_rebalance.balanceFactor() == +2
+            if node_to_rebalance.left.balanceFactor() >= 0:
+                B = A.left
+                C = B.left
+                assert not A is None and not B is None and not C is None
+                A.left = B.right
+                if A.left:
+                    A.left.parent = A
+                B.right = A
+                A.parent = B
+                if F is None:
+                    self.root = B
+                    self.root.parent = None
+                else:
+                    if F.right == A:
+                        F.right = B
+                    else:
+                        F.left = B
+                    B.parent = F
+                self._recomputeHeights(A)
+                self._recomputeHeights(B.parent)
+            else:
+                B = A.left
+                C = B.right
+                assert not A is None and not B is None and not C is None
+                A.left = C.right
+                if A.left:
+                    A.left.parent = A
+                B.right = C.left
+                if B.right:
+                    B.right.parent = B
+                C.left = B
+                B.parent = C
+                C.right = A
+                A.parent = C
+                if F is None:
+                    self.root = C
+                    self.root.parent = None
+                else:
+                    if F.right == A:
+                        F.right = C
+                    else:
+                        F.left = C
+                    C.parent = F
+                self._recomputeHeights(A)
+                self._recomputeHeights(B)
+        self.rebalance_count += 1
+
+    def _recomputeHeights(self, start_from_node):
+        changed = True
+        node = start_from_node
+        while node and changed:
+            old_height = node.height
+            node.height = (
+                node.maxChildrenHeight() + 1 if (node.right or node.left) else 0
+            )
+            changed = node.height != old_height
+            node = node.parent
+
+    def search(self, key):
+        """
+        Search a AVLNode satisfies AVLNode.val = key.
+        if found return AVLNode, else return None.
+        """
+        return self._dfsSearch(self.root, key)
+
+    def _dfsSearch(self, currentNode, key):
+        """
+        Helper function to search a key in AVLTree.
+        """
+        if currentNode is None:
+            return None
+        elif currentNode.val == key:
+            return currentNode
+        elif currentNode.val > key:
+            return self._dfsSearch(currentNode.left, key)
+        else:
+            return self._dfsSearch(currentNode.right, key)
+
+    def delete(self, key):
+        """
+        Delete a key from AVLTree
+        """
+        # first find
+        node = self.search(key)
+
+        if not node is None:
+            self.nodes_count -= 1
+            #     There are three cases:
+            #
+            #     1) The node is a leaf.  Remove it and return.
+            #
+            #     2) The node is a branch (has only 1 child). Make the pointer to this node
+            #        point to the child of this node.
+            #
+            #     3) The node has two children. Swap items with the successor
+            #        of the node (the smallest item in its right subtree) and
+            #        delete the successor from the right subtree of the node.
+            if node.isLeaf():
+                self._removeLeaf(node)
+            elif (bool(node.left)) ^ (bool(node.right)):
+                self._removeBranch(node)
+            else:
+                assert (node.left) and (node.right)
+                self._swapWithSuccessorAndRemove(node)
+
+    def _removeLeaf(self, node):
+        parent = node.parent
+        if parent:
+            if parent.left == node:
+                parent.left = None
+            else:
+                assert parent.right == node
+                parent.right = None
+            self._recomputeHeights(parent)
+        else:
+            self.root = None
+        del node
+        # rebalance
+        node = parent
+        while node:
+            if not node.balanceFactor() in [-1, 0, 1]:
+                self._rebalance(node)
+            node = node.parent
+
+    def _removeBranch(self, node):
+        parent = node.parent
+        if parent:
+            if parent.left == node:
+                parent.left = node.right if node.right else node.left
+            else:
+                assert parent.right == node
+                parent.right = node.right if node.right else node.left
+            if node.left:
+                node.left.parent = parent
+            else:
+                assert node.right
+                node.right.parent = parent
+            self._recomputeHeights(parent)
+        del node
+        # rebalance
+        node = parent
+        while node:
+            if not node.balanceFactor() in [-1, 0, 1]:
+                self._rebalance(node)
+            node = node.parent
+
+    def _swapWithSuccessorAndRemove(self, node):
+        successor = self.findSmallest(node.right)
+        self._swapNodes(node, successor)
+        assert node.left is None
+        if node.height == 0:
+            self._removeLeaf(node)
+        else:
+            self._removeBranch(node)
+
+    def _swapNodes(self, node1, node2):
+        assert node1.height > node2.height
+        parent1 = node1.parent
+        leftChild1 = node1.left
+        rightChild1 = node1.right
+        parent2 = node2.parent
+        assert not parent2 is None
+        assert parent2.left == node2 or parent2 == node1
+        leftChild2 = node2.left
+        assert leftChild2 is None
+        rightChild2 = node2.right
+
+        # swap heights
+        tmp = node1.height
+        node1.height = node2.height
+        node2.height = tmp
+
+        if parent1:
+            if parent1.left == node1:
+                parent1.left = node2
+            else:
+                assert parent1.right == node1
+                parent1.right = node2
+            node2.parent = parent1
+        else:
+            self.root = node2
+            node2.parent = None
+
+        node2.left = leftChild1
+        leftChild1.parent = node2
+        node1.left = leftChild2  # None
+        node1.right = rightChild2
+        if rightChild2:
+            rightChild2.parent = node1
+        if not (parent2 == node1):
+            node2.right = rightChild1
+            rightChild1.parent = node2
+            parent2.left = node1
+            node1.parent = parent2
+        else:
+            node2.right = node1
+            node1.parent = node2
+
+def hull():
+    set_of_points: list[Point] = []
+    for _ in range(100):
+        x: int
+        y: int
+        x, y = random.randint(1, 100), random.randint(1, 100)
+        set_of_points.append(Point(x, y))
+    
+    hull: ConvexHull = ConvexHull(set_of_points, alg="gift_wrapping")
+    return hull
+
+def plot_polygon(polygon: list[Segment], color: str):
+    """
+    Imprime as arestas de um polígono usando a cor `color`
+    """
+    for edge in polygon:
+        plt.plot([edge.p0.x, edge.p1.x], [edge.p0.y, edge.p1.y], color, linewidth=0.5)
+
+def separable():
+    polygon_a = hull().convex_hull
+    polygon_b = hull().convex_hull
+    _, ax = plt.subplots()
+    ax = cast(plt.Axes, ax)
+    ax.grid(which="both", color="grey", linewidth=0.5, linestyle="-", alpha=0.2)
+    plot_polygon(polygon_a, "blue")
+    plot_polygon(polygon_b, "green")
+    plt.savefig("polígonos.png")
+    line_sweeper = LineSweep()
+    return not line_sweeper.do_polygons_intersect(polygon_a,polygon_b)
+
+
+
+
+if __name__ == "__main__":
+    print("""O programa tem duas funções
+          1. A partir de um conjunto de pontos aleatório gerar uma envoltória convexa
+          2. Verificar se dois polígonos são linearmente separáveis.
+          Para visualizar isso, são geradas duas figuras: envoltória.png e polígonos.png
+          É impresso na tela se os polígonos são linearmente separáveis ou não.""")
+    convex_hull = hull()
+    convex_hull.plot()
+    if separable():
+        print("Polígonos são linearmente separáveis")
+    else:
+        print("Polígonos não são linearmente separáveis")
