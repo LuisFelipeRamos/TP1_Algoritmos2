02eceb63c9575150310ca59438b6ae9ed4761c6d
Igor <igorlfs@ufmg.br>
2022-10-30 12:05:02 -0300

    refactor(main): imports, formatação e descrição

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
 import argparse
 
-from src.data_import import *
-
 import src.globals as g
+from src.data_import import (
+    check_banana,
+    check_glass,
+    check_haberman,
+    check_iris,
+    check_magic,
+    check_mammographic,
+    check_newthyroid,
+    check_segment,
+    check_titanic,
+    check_wine,
+)
 
 
 def run() -> None:
     """
-    Roda o programa principal.
+    Roda o programa principal: analisa um dataset imprimindo suas métricas
+    se for separável ou imprimindo que não é separável
     """
 
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
@@ -22,25 +33,25 @@ def run() -> None:
     )
     args: argparse.Namespace = parser.parse_args()
 
-    if (args.data == g.BANANA):
+    if args.data == g.BANANA:
         check_banana("data/banana.dat")
-    elif(args.data == g.GLASS):
+    elif args.data == g.GLASS:
         check_glass("data/glass.dat")
-    elif(args.data == g.HABERMAN):
+    elif args.data == g.HABERMAN:
         check_haberman("data/haberman.dat")
-    elif(args.data == g.IRIS):
+    elif args.data == g.IRIS:
         check_iris("data/iris.dat")
-    elif(args.data == g.MAGIC):
+    elif args.data == g.MAGIC:
         check_magic("data/magic.dat")
-    elif(args.data == g.MAMMOGRAPHIC):
+    elif args.data == g.MAMMOGRAPHIC:
         check_mammographic("data/mammographic.dat")
-    elif(args.data == g.NEWTHYROID):
+    elif args.data == g.NEWTHYROID:
         check_newthyroid("data/newthyroid.dat")
-    elif(args.data == g.SEGMENT):
+    elif args.data == g.SEGMENT:
         check_segment("data/segment.dat")
-    elif(args.data == g.TITANIC):
+    elif args.data == g.TITANIC:
         check_titanic("data/titanic.dat")
-    elif(args.data == g.WINE):
+    elif args.data == g.WINE:
         check_wine("data/wine.dat")
     else:
         print("Esse dataset não existe no nosso trabalho!")
